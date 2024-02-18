cf8776658d032c91a21aaee2508b5f6dae4a85e6
ManavNarangg <108012502+ManavNarangg@users.noreply.github.com>
2024-02-18 13:03:58 +0530

    Update MyOrder.js
    My change

--- frontend/src/screens/MyOrder.js
+++ frontend/src/screens/MyOrder.js
@@ -7,7 +7,7 @@ export default function MyOrder() {
 
   const fetchMyOrder = async () => {
     console.log(localStorage.getItem("userEmail"));
-    await fetch("http://localhost:5000/api/myOrder", {
+    await fetch("https://gofoodapp-kj7m.onrender.com/api/myOrder", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -32,74 +32,75 @@ export default function MyOrder() {
       </div>
 
       <div className="container">
-        <div className="row">
-          {orderData !== {}
-            ? Array(orderData).map((data) => {
-                return data.orderData
-                  ? data.orderData.order_data
-                      .slice(0)
-                      .reverse()
-                      .map((item) => {
-                        return item.map((arrayData) => {
-                          return (
-                            <div>
-                              {arrayData.Order_date ? (
-                                <div className="m-auto mt-5">
-                                  {(data = arrayData.Order_date)}
-                                  <hr />
-                                </div>
-                              ) : (
-                                <div className="col-12 col-md-6 col-lg-3">
-                                  <div
-                                    className="card mt-3"
-                                    style={{
-                                      width: "16rem",
-                                      maxHeight: "360px",
-                                    }}
-                                  >
-                                    <img
-                                      src={arrayData.img}
-                                      className="card-img-top"
-                                      alt="..."
-                                      style={{
-                                        height: "120px",
-                                        objectFit: "fill",
-                                      }}
-                                    />
-                                    <div className="card-body">
-                                      <h5 className="card-title">
-                                        {arrayData.name}
-                                      </h5>
-                                      <div
-                                        className="container w-100 p-0"
-                                        style={{ height: "38px" }}
-                                      >
-                                        <span className="m-1">
-                                          {arrayData.qty}
-                                        </span>
-                                        <span className="m-1">
-                                          {arrayData.size}
-                                        </span>
-                                        <span className="m-1">{data}</span>
-                                        <div className=" d-inline ms-2 h-100 w-20 fs-5">
-                                          ₹{arrayData.price}/-
-                                        </div>
-                                      </div>
-                                    </div>
+  <div className="row">
+    {Object.keys(orderData).length !== 0
+      ? Array(orderData).map((data) => {
+          return data.orderData
+            ? data.orderData.order_data
+                .slice(0)
+                .reverse()
+                .map((item) => {
+                  return item.map((arrayData) => {
+                    return (
+                      <div>
+                        {arrayData.Order_date ? (
+                          <div className="m-auto mt-5">
+                            {(data = arrayData.Order_date)}
+                            <hr />
+                          </div>
+                        ) : (
+                          <div className="col-12 col-md-6 col-lg-3">
+                            <div
+                              className="card mt-3"
+                              style={{
+                                width: "16rem",
+                                maxHeight: "360px",
+                              }}
+                            >
+                              <img
+                                src={arrayData.img}
+                                className="card-img-top"
+                                alt="..."
+                                style={{
+                                  height: "120px",
+                                  objectFit: "fill",
+                                }}
+                              />
+                              <div className="card-body">
+                                <h5 className="card-title">
+                                  {arrayData.name}
+                                </h5>
+                                <div
+                                  className="container w-100 p-0"
+                                  style={{ height: "38px" }}
+                                >
+                                  <span className="m-1">
+                                    {arrayData.qty}
+                                  </span>
+                                  <span className="m-1">
+                                    {arrayData.size}
+                                  </span>
+                                  <span className="m-1">{data}</span>
+                                  <div className=" d-inline ms-2 h-100 w-20 fs-5">
+                                    ₹{arrayData.price}/-
                                   </div>
                                 </div>
-                              )}
+                              </div>
                             </div>
-                          );
-                        });
-                      })
-                  : "";
-              })
-            : ""}
-        </div>
-      </div>
+                          </div>
+                        )}
+                      </div>
+                    );
+                  });
+                })
+            : "";
+        })
+      : ""}
+  </div>
+</div>
 
-      <Footer />
+<Footer />
+
     </div>
   );
 }
